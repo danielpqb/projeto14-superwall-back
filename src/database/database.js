6cb91822ec7b14031b009365e7ede12110545c79
danielpqb <danielpqb@gmail.com>
2022-09-14 14:05:29 -0300

    Creating Schemas

---
+++ src/database/database.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DB_URI);
+try {
+  await mongoClient.connect();
+  console.log("Conectado ao MongoDB");
+} catch (error) {
+  console.log(error.message);
+}
+const db = mongoClient.db("superwall");
+
+export { db };
