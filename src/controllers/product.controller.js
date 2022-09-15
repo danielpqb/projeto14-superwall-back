66e3ecbcb3564d31f1db34a0ffa6286ccf1f9ebc
danielpqb <danielpqb@gmail.com>
2022-09-15 12:27:32 -0300

    getOne product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.js";
 async function getAll(req, res) {
   try {
     const products = await db.collection("products").find().toArray();
-    res.status(201).send(products);
+    res.status(200).send(products);
     return;
   } catch (error) {
     res.status(500).send(error.message);
@@ -11,4 +11,18 @@ async function getAll(req, res) {
   }
 }
 
-export { getAll };
+async function getOne(req, res) {
+  try {
+    const product = await db
+      .collection("products")
+      .findOne({ id: parseInt(req.params.id) });
+    console.log(product);
+    res.status(200).send(product);
+    return;
+  } catch (error) {
+    res.status(500).send(error.message);
+    return;
+  }
+}
+
+export { getAll, getOne };
